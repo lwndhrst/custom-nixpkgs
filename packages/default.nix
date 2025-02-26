2a616803a8ec9377f90fc2899d7c94e5a97aed2d
Leon Windhorst <leon@windhorst.dev>
2025-02-26 12:59:04 +0100

    Add rose-pine override script

--- packages/default.nix
+++ packages/default.nix
@@ -1,22 +1,26 @@
 { pkgs, ... }:
 
 {
-  amdsmi                  = pkgs.callPackage ./amdsmi {};
+  amdsmi                = pkgs.callPackage ./amdsmi {};
   cgv = {
-    master                = pkgs.callPackage ./cgv { gitCommit = "master"; };
-    develop               = pkgs.callPackage ./cgv { gitCommit = "develop"; };
-    font-fix              = pkgs.callPackage ./cgv { gitCommit = "font-fix"; };
+    master              = pkgs.callPackage ./cgv { gitCommit = "master"; };
+    develop             = pkgs.callPackage ./cgv { gitCommit = "develop"; };
+    font-fix            = pkgs.callPackage ./cgv { gitCommit = "font-fix"; };
   };
-  cinder                  = pkgs.callPackage ./cinder {};
-  fcitx5-rose-pine-edit   = pkgs.callPackage ./fcitx5-rose-pine-edit {};
-  nitch                   = pkgs.callPackage ./nitch {};
-  odin                    = pkgs.callPackage ./odin {};
-  ols                     = pkgs.callPackage ./ols {};
-  path-of-building        = pkgs.callPackage ./path-of-building {};
-  sddm-rose-pine          = pkgs.callPackage ./sddm-rose-pine {};
+  cinder                = pkgs.callPackage ./cinder {};
+  fcitx5-rose-pine-edit = pkgs.callPackage ./fcitx5-rose-pine-edit {};
+  nitch                 = pkgs.callPackage ./nitch {};
+  odin                  = pkgs.callPackage ./odin {};
+  ols                   = pkgs.callPackage ./ols {};
+  path-of-building      = pkgs.callPackage ./path-of-building {};
+  sddm-rose-pine        = pkgs.callPackage ./sddm-rose-pine {};
+
+  scripts = {
+    override-rose-pine-colors = pkgs.callPackage ./scripts/override-rose-pine-colors {};
+  };
 
   vimPlugins = {
-    harpoon               = pkgs.callPackage ./vim/plugins/harpoon {};
-    ranger-nvim           = pkgs.callPackage ./vim/plugins/ranger-nvim {};
+    harpoon     = pkgs.callPackage ./vim/plugins/harpoon {};
+    ranger-nvim = pkgs.callPackage ./vim/plugins/ranger-nvim {};
   };
 }
---
+++ packages/scripts/override-rose-pine-colors/default.nix
@@ -0,0 +1,42 @@
+{ writeShellApplication }:
+
+let
+  default-colors = {
+    base =           "#191724";
+    surface =        "#1f1d2e";
+    overlay =        "#26233a";
+    muted =          "#6e6a86";
+    subtle =         "#908caa";
+    text =           "#e0def4";
+    highlight-low =  "#21202e";
+    highlight-med =  "#403d52";
+    highlight-high = "#524f67";
+  };
+  custom-colors = {
+    base =           "#040e14";
+    surface =        "#0a161d";
+    overlay =        "#0b1e28";
+    muted =          "#506471";
+    subtle =         "#708593";
+    text =           "#c8d4dc";
+    highlight-low =  "#11181d";
+    highlight-med =  "#29363f";
+    highlight-high = "#384853";
+  };
+
+in writeShellApplication {
+  name = "override-rose-pine-colors";
+  text = ''
+    FILE=$1
+
+    sed -i 's/${default-colors.base}/${custom-colors.base}/g' "$FILE"
+    sed -i 's/${default-colors.surface}/${custom-colors.surface}/g' "$FILE"
+    sed -i 's/${default-colors.overlay}/${custom-colors.overlay}/g' "$FILE"
+    sed -i 's/${default-colors.muted}/${custom-colors.muted}/g' "$FILE"
+    sed -i 's/${default-colors.subtle}/${custom-colors.subtle}/g' "$FILE"
+    sed -i 's/${default-colors.text}/${custom-colors.text}/g' "$FILE"
+    sed -i 's/${default-colors.highlight-low}/${custom-colors.highlight-low}/g' "$FILE"
+    sed -i 's/${default-colors.highlight-med}/${custom-colors.highlight-med}/g' "$FILE"
+    sed -i 's/${default-colors.highlight-high}/${custom-colors.highlight-high}/g' "$FILE"
+  '';
+}
