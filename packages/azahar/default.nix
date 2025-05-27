ffe6f240bd39c9ddbdeae4caf29dd2cb3bdf7912
Leon Windhorst <leon@windhorst.dev>
2025-05-27 10:14:30 +0200

    Add azahar emulator desktop file

--- packages/azahar/default.nix
+++ packages/azahar/default.nix
@@ -1,21 +1,29 @@
 { lib
 , fetchurl
 , appimageTools
+, makeDesktopItem
 }:
 
-appimageTools.wrapType2 rec {
+let
   pname = "azahar";
   version = "2121.1";
-
   src = fetchurl {
     url = "https://github.com/azahar-emu/azahar/releases/download/${version}/azahar.AppImage";
     sha256 = "L7/VP9WGwGy3iRQ8i3DK5JR/Ag3A1NAXhKhWjyis+LQ=";
   };
+  desktopItem = makeDesktopItem {
+    name = pname;
+    exec = pname;
+    desktopName = "Azahar Emulator";
+    startupWMClass = "azahar";
+  };
 
-  extraInstallCommands = ''
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace-fail 'Exec=AppRun' 'Exec=${meta.mainProgram}'
-  '';
+in appimageTools.wrapType2 rec {
+  inherit pname version src;
+
+  extraPkgs = pkgs: [
+    desktopItem
+  ];
 
   meta = with lib; {
     description = "An open-source 3DS emulator project based on Citra.";
